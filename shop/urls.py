ff71b3dccf6c9560fd65e855bb2552c13172622f
mojanazwa <manuelaguerrasamaniegouni@gmail.com>
2024-12-17 22:32:26 +0100

    Dodalam widok szczegolowy dla winyla i linki w liscie

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('vinyls/', views.vinyl_list, name='vinyl_list'),
+    path('vinyls/<int:pk>/', views.vinyl_detail, name='vinyl_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Vinyl
 
 def vinyl_list(request):
@@ -7,3 +7,10 @@ def vinyl_list(request):
     """
     vinyls = Vinyl.objects.all()  # Pobierz wszystkie obiekty z modelu Vinyl
     return render(request, 'shop/vinyl_list.html', {'vinyls': vinyls})
+
+def vinyl_detail(request, pk):
+    """
+    Widok szczegółowy dla pojedynczego winyla.
+    """
+    vinyl = get_object_or_404(Vinyl, pk=pk)  # Pobierz obiekt lub zwróć 404
+    return render(request, 'shop/vinyl_detail.html', {'vinyl': vinyl})
