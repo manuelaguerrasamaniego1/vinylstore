6b6a7a64b45ca08134e9e5e507dc3b89cb2864ab
mojanazwa <manuelaguerrasamaniegouni@gmail.com>
2024-12-17 23:10:38 +0100

    Usuwanie i edycja winyli

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('vinyls/', views.vinyl_list, name='vinyl_list'),
-    path('vinyls/<int:pk>/', views.vinyl_detail, name='vinyl_detail'),
+    path('', views.vinyl_list, name='vinyl_list'),  # Używamy funkcji vinyl_list
+    path('vinyls/<int:pk>/', views.vinyl_detail, name='vinyl_detail'),  # Używamy funkcji vinyl_detail
+    path('vinyls/<int:pk>/edit/', views.VinylUpdateView.as_view(), name='vinyl_edit'),
+    path('vinyls/<int:pk>/delete/', views.VinylDeleteView.as_view(), name='vinyl_delete'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Vinyl
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, DeleteView
 
+# Widok listy winyli
 def vinyl_list(request):
     """
     Widok zwracający listę wszystkich winyli.
@@ -8,9 +11,23 @@ def vinyl_list(request):
     vinyls = Vinyl.objects.all()  # Pobierz wszystkie obiekty z modelu Vinyl
     return render(request, 'shop/vinyl_list.html', {'vinyls': vinyls})
 
+# Widok szczegółowy dla pojedynczego winyla
 def vinyl_detail(request, pk):
     """
     Widok szczegółowy dla pojedynczego winyla.
     """
     vinyl = get_object_or_404(Vinyl, pk=pk)  # Pobierz obiekt lub zwróć 404
     return render(request, 'shop/vinyl_detail.html', {'vinyl': vinyl})
+
+# Widok edycji winyla
+class VinylUpdateView(UpdateView):
+    model = Vinyl
+    fields = ['title', 'artist', 'genre', 'release_year', 'price']
+    template_name = 'shop/vinyl_edit.html'
+    success_url = reverse_lazy('vinyl_list')  # Po edycji wraca na listę winyli
+
+# Widok usuwania winyla
+class VinylDeleteView(DeleteView):
+    model = Vinyl
+    template_name = 'shop/vinyl_delete.html'
+    success_url = reverse_lazy('vinyl_list')  # Po usunięciu wraca na listę winyli
